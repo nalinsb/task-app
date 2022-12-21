5ab763a8695d34b217700fa1b7aedfcb6e90da0a
nalinsb <nalinsanjeewa1997@gmail.com>
2022-12-21 16:59:17 +0530

    Finalize the UserDAOImpl

--- src/main/java/lk/ijse/dep9/app/WebRootConfig.java
+++ src/main/java/lk/ijse/dep9/app/WebRootConfig.java
@@ -3,35 +3,47 @@ package lk.ijse.dep9.app;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.jndi.JndiObjectFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.context.annotation.RequestScope;
 
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.SQLException;
 
 @Configuration
+@EnableTransactionManagement
 public class WebRootConfig {
 
     @Bean
-    public JndiObjectFactoryBean dataSource(){
+    public JndiObjectFactoryBean dataSource() {
         JndiObjectFactoryBean jndi = new JndiObjectFactoryBean();
         jndi.setJndiName("java:comp/env/jdbc/task-app");
         jndi.setExpectedType(DataSource.class);
         return jndi;
     }
 
-    @Bean(destroyMethod = "close")
+    @Bean
     @RequestScope
-    public Connection connection(DataSource dataSource) throws SQLException {
-        return dataSource.getConnection();
+    public Connection connection(DataSource ds) {
+        return DataSourceUtils.getConnection(ds);
     }
 
     @Bean
-    public ModelMapper modelMapper(){
+    public JdbcTemplate jdbcTemplate(DataSource ds){
+        return new JdbcTemplate(ds);
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource ds) {
+        return new DataSourceTransactionManager(ds);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
         return new ModelMapper();
     }
 }
--- src/main/java/lk/ijse/dep9/app/dao/custom/impl/UserDAOImpl.java
+++ src/main/java/lk/ijse/dep9/app/dao/custom/impl/UserDAOImpl.java
@@ -4,6 +4,7 @@ import lk.ijse.dep9.app.dao.custom.UserDAO;
 import lk.ijse.dep9.app.dao.util.ConnectionUtil;
 import lk.ijse.dep9.app.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.sql.Connection;
@@ -18,96 +19,56 @@ import java.util.Optional;
 public class UserDAOImpl implements UserDAO {
 
     private final Connection connection;
+    private final JdbcTemplate jdbc;
 
-    public UserDAOImpl(Connection connection) {
+    public UserDAOImpl(Connection connection, JdbcTemplate jdbc) {
         this.connection = connection;
+        this.jdbc = jdbc;
     }
 
     @Override
     public User save(User user) {
-        try {
-            PreparedStatement stm = connection.
-                    prepareStatement("INSERT INTO User (username, full_name, password) VALUES (?, ?, ?)");
-            stm.setString(1, user.getUsername());
-            stm.setString(2, user.getFullName());
-            stm.setString(3, user.getPassword());
-            stm.executeUpdate();
-            return user;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        jdbc.update("INSERT INTO User (username, full_name, password) VALUES (?, ?, ?)",
+                user.getUsername(), user.getFullName(), user.getPassword());
+        return user;
     }
 
     @Override
     public void update(User user) {
-        try {
-            PreparedStatement stm = connection.
-                    prepareStatement("UPDATE User SET full_name=?, password=? WHERE username=?");
-            stm.setString(1, user.getFullName());
-            stm.setString(2, user.getPassword());
-            stm.setString(3, user.getUsername());
-            stm.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        jdbc.update("UPDATE User SET full_name=?, password=? WHERE username=?",
+                user.getFullName(), user.getPassword(), user.getUsername());
     }
 
     @Override
     public void deleteById(String username) {
-        try {
-            PreparedStatement stm = connection.prepareStatement("DELETE FROM User WHERE username=?");
-            stm.setString(1, username);
-            stm.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        jdbc.update("DELETE FROM User WHERE username=?", username);
     }
 
     @Override
     public Optional<User> findById(String username) {
-        try {
-            PreparedStatement stm = connection.
-                    prepareStatement("SELECT full_name, password FROM User WHERE username=?");
-            stm.setString(1, username);
-            ResultSet rst = stm.executeQuery();
-            if (rst.next()) {
+        return jdbc.query("SELECT full_name, password FROM User WHERE username=?", rst -> {
                 return Optional.of(new User(username,
                         rst.getString("password"),
                         rst.getString("full_name")));
-            }
-            return Optional.empty();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        },username);
     }
 
     @Override
     public List<User> findAll() {
-        try {
+        return jdbc.query("SELECT * FROM User", rst -> {
             List<User> userList = new ArrayList<>();
-            PreparedStatement stm = connection.prepareStatement("SELECT * FROM User");
-            ResultSet rst = stm.executeQuery();
             while (rst.next()) {
                 userList.add(new User(rst.getString("username"),
                         rst.getString("password"),
                         rst.getString("full_name")));
             }
             return userList;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        });
     }
 
     @Override
     public long count() {
-        try {
-            PreparedStatement stm = connection.prepareStatement("SELECT COUNT(username) FROM User");
-            ResultSet rst = stm.executeQuery();
-            rst.next();
-            return rst.getLong(1);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        return jdbc.queryForObject("SELECT COUNT(username) FROM User", Long.class);
     }
 
     @Override
